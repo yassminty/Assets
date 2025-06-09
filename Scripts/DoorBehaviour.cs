f5df849c8f08ca41d5aee597091f565f84452b9a
yassminty <s10247871@connect.np.edu.sg>
2025-06-09 14:07:49 +0800

    Added Scripts & Audio
    created some scripts & added audio files to hopefully finish this project T-T

---
+++ Scripts/DoorBehaviour.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    public void Interact()
+    {
+        Vector3 doorRotation = transform.eulerAngles;
+        doorRotation.y += 90f; //will rotate door 90 degrees
+        transform.eulerAngles = doorRotation; //is to apply the rotation
+    }
+}
